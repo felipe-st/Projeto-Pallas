2243152f2400a84cd04e2ac878baa46e25b554b2
Felipe Taleires <jfelipetaleires@hotmail.com>
2023-03-18 20:08:12 -0300

    del_lista

--- api/v1/endpoints/lista.py
+++ api/v1/endpoints/lista.py
@@ -36,3 +36,41 @@ async def get_lista(usuario_logado: UsuariosModel = Depends(get_current_user), d
         return lista_completa
 
 
+#PUT lista
+@router.put('/{linha_lista_id}', response_model=ListaSchemaBase, status_code=status.HTTP_202_ACCEPTED)
+async def put_lista(linha_lista_id: int, lista: ListaSchemaBase, db: AsyncSession = Depends(get_session), usuario_logado: UsuariosModel = Depends(get_current_user)):
+    async with db as session:
+        query = select(ListaModel).filter(ListaModel.id == linha_lista_id).filter(ListaModel.usuario_id == usuario_logado.id)
+        result = await session.execute(query)
+        lista_up: ListaModel = result.scalars().unique().one_or_none()
+
+        if lista_up:
+            if lista_up.produto:
+                lista_up.produto = lista.produto
+            if lista_up.quantidade:
+                lista_up.quantidade = lista.quantidade
+            if lista_up.preco:
+                lista_up.preco = lista.preco
+
+            await session.commit()
+
+            return lista_up
+        else:
+            raise HTTPException(detail='Item da lista não encontrada', status_code=status.HTTP_404_NOT_FOUND)
+
+
+#DELETE item lista:
+@router.delete('/{linha_lista_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item_lista(linha_lista_id: int, db:AsyncSession = Depends(get_session), usuario_logado: UsuariosModel = Depends(get_current_user)):
+    async with db as session:
+        query = select(ListaModel).filter(ListaModel.id == linha_lista_id).filter(ListaModel.usuario_id == usuario_logado.id)
+        result = await session.execute(query)
+        item_del: ListaModel = result.scalars().unique().one_or_none()
+
+        if item_del:
+            await session.delete(item_del)
+            await session.commit()
+
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise HTTPException(detail='item não encontrado', status_code=status.HTTP_404_NOT_FOUND)
