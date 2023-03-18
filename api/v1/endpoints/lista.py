f8193afe339c045be3466604a9bfeddbdb03affe
Felipe Taleires <jfelipetaleires@hotmail.com>
2023-03-18 19:28:57 -0300

    get_lista

--- api/v1/endpoints/lista.py
+++ api/v1/endpoints/lista.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from fastapi import APIRouter, status, Depends, HTTPException, Response
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 
 from core.deps import get_current_user, get_session
 from models.usuarios_model import UsuariosModel
@@ -13,10 +16,23 @@ router = APIRouter()
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=ListaSchemaBase)
 async def post_lista(lista: ListaSchemaBase, usuario_logado: UsuariosModel = Depends(get_current_user), db: AsyncSession=Depends(get_session)):
     novo_item_lista: ListaModel = ListaModel(produto=lista.produto, quantidade=lista.quantidade, preco=lista.preco, usuario_id=usuario_logado.id)
-
+    print(f'Usuario logado: {usuario_logado.id}')
     novo_item_lista.preco = novo_item_lista.quantidade*novo_item_lista.preco
 
     db.add(novo_item_lista)
     await db.commit()
 
     return novo_item_lista
+
+
+#GET lista
+@router.get('/', response_model=List[ListaSchemaBase])
+async def get_lista(usuario_logado: UsuariosModel = Depends(get_current_user), db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(ListaModel).filter(ListaModel.usuario_id == usuario_logado.id)
+        result = await session.execute(query)
+        lista_completa: List[ListaModel] = result.scalars().unique().all()
+
+        return lista_completa
+
+
